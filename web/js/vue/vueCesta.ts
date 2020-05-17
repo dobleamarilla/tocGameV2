8d81df1873e216a2a6d7f21a3292155d0a64ae7f
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2020-05-18 01:47:45 +0200

    Visual pantalla principal OK

---
+++ web/js/vue/vueCesta.ts
@@ -0,0 +1,72 @@
+var vueCesta = new Vue({
+    el: '#vueCesta',
+    template: 
+    /*html*/`
+    <div class="table-responsive estiloCesta">
+        <table class="table table-hover fuenteIvan colorFuente" id="job-table">
+            <thead>
+                <tr>
+                <th scope="col">Productos</th>
+                <th scope="col">Unidades</th>
+                <th scope="col">Precio</th>
+                </tr>
+            </thead>
+            <tbody class="tableBody">
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+                <tr>
+                <td>CocaCola Light</td>
+                <td>3</td>
+                <td>5,40</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+    data () {
+      return {
+          mensaje: '',
+      }
+    },
+    methods: {
+
+    }
+  });
