fd4a8f2c3d63c2bcb351b143785f628c62ed22cc
Eze <ezequielucho@gmail.com>
2020-05-30 08:22:34 +0200

    Clientes vinculados

--- web/js/vue/vueClientes.js
+++ web/js/vue/vueClientes.js
@@ -14,7 +14,7 @@ var vueClientes = new Vue({
                 <div class="row">
                     <div class="col-md-12">
                         <div class="md-form mt-0">
-                            <input class="form-control" type="text" v-model="busqueda" placeholder="Nombre del cliente">
+                            <input id="inputBusqueda" class="form-control" type="text" v-model="busqueda" placeholder="Nombre del cliente">
                         </div>
                     <div class="table-responsive" style="height: 400px;">
                          <table class="table table-striped">
@@ -56,6 +56,7 @@ var vueClientes = new Vue({
     methods: {
         abrirModal() {
             $('#modalClientes').modal();
+            document.getElementById('inputBusqueda').focus();
         },
         cerrarModal() {
             $('#modalClientes').modal('hide');
