c6f1cb7d246395be0f290e2baab5733c18968944
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2021-02-20 09:15:41 +0100

    Fix footer

--- web/js/vue/vueInfoFooter.ts
+++ web/js/vue/vueInfoFooter.ts
@@ -3,7 +3,7 @@ var vueInfoFooter = new Vue({
     template: 
     /*html*/`
 <div>
-    <div style="position: fixed; bottom: 0; right: 0;">{{hora}}&nbsp;&nbsp;&nbsp;Versión: {{version}}&nbsp;&nbsp;&nbsp;Internet <img v-bind:src="'assets/imagenes/'+internet" alt="estadoInternet">&nbsp;&nbsp;&nbsp;Cestas abiertas: {{numCestas}} &nbsp;&nbsp;&nbsp; Nombre tienda: {{nombreTienda}}</div>
+    <div style="position: fixed; bottom: 0; right: 0;">Versión: {{version}}&nbsp;&nbsp;&nbsp;Internet <img v-bind:src="'assets/imagenes/'+internet" alt="estadoInternet">&nbsp;&nbsp;&nbsp;Cestas abiertas: {{numCestas}} &nbsp;&nbsp;&nbsp; Nombre tienda: {{nombreTienda}}</div>
 </div>
     `,
     data () 
@@ -12,8 +12,7 @@ var vueInfoFooter = new Vue({
           numCestas: 1,
           nombreTienda: 'SIN LICENCIA',
           internet: 'offline.png',
-          version: '',
-          hora: ''
+          version: ''
       }
     },
     methods: {
@@ -25,8 +24,6 @@ var vueInfoFooter = new Vue({
         {
             this.nombreTienda = toc.getParametros().nombreTienda;
             this.version = ipcRenderer.sendSync('get-version');
-            this.hora = toc.horaActual();
-            setInterval(this.getParametros, 1000);
         },
         hayInternet(res)
         {
