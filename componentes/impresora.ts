95af98fb31f9a3bdb843c2f0e01e620c16bb466e
itsS4nty <82sao82@gmail.com>
2021-04-08 13:07:41 +0200

    Visor

--- componentes/impresora.ts
+++ componentes/impresora.ts
@@ -306,12 +306,11 @@ var testEze = function (event, texto)
 var mostrarVisor = function(event, data) {
     var eur = String.fromCharCode(128);
     var limitNombre = 13;
-    console.log(data.total.length);
-    console.log(typeof data.total);
-    if(data.total.length == 2) limitNombre = 16;
-    else if(data.total.length == 3) limitNombre = 15;
-    else if(data.total.length == 4) limitNombre = 14;
-    else if(data.total.length == 6) limitNombre = 12;
+    var lengthTotal = (data.total).toString();
+    if(lengthTotal.length == 2) limitNombre = 16;
+    else if(lengthTotal.length == 3) limitNombre = 15;
+    else if(lengthTotal.length == 4) limitNombre = 14;
+    else if(lengthTotal.length == 6) limitNombre = 12;
     // Limito el texto a 14, ya que la línea completa tiene 20 espacios. (1-14 -> artículo, 15 -> espacio en blanco, 16-20 -> precio)
     var datosExtra = data.dependienta.substring(0, limitNombre) + " " + data.total + eur; 
     if(datosExtra.length <= 2) datosExtra = "";
