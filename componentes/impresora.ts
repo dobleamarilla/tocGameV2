b6643629b888b7399314fba2aee2d3edecdf3637
itsS4nty <82sao82@gmail.com>
2021-04-08 14:09:19 +0200

    Visor

--- componentes/impresora.ts
+++ componentes/impresora.ts
@@ -305,21 +305,25 @@ var testEze = function (event, texto)
 }
 var mostrarVisor = function(event, data) {
     var eur = String.fromCharCode(128);
-    var limitNombre = 13;
-    var lengthTotal = '0';
+    var limitNombre = 0;
+    var lengthTotal = '';
+    var datosExtra = '';
     if(data.total !== undefined) {
+        datosExtra = data.dependienta.substring(0, limitNombre) + " " + data.total + eur; 
         lengthTotal = (data.total).toString();
-        if(lengthTotal.length == 2) limitNombre = 16;
+        if(lengthTotal.length == 1) limitNombre = 17;
+        else if(lengthTotal.length == 2) limitNombre = 16;
         else if(lengthTotal.length == 3) limitNombre = 15;
         else if(lengthTotal.length == 4) limitNombre = 14;
+        else if(lengthTotal.length == 5) limitNombre = 13;
         else if(lengthTotal.length == 6) limitNombre = 12;
+        else if(lengthTotal.length == 7) limitNombre = 11;
     }
-    // Limito el texto a 14, ya que la línea completa tiene 20 espacios. (1-14 -> artículo, 15 -> espacio en blanco, 16-20 -> precio)
-    var datosExtra = data.dependienta.substring(0, limitNombre) + " " + data.total + eur; 
     if(datosExtra.length <= 2){
         datosExtra = ""; 
         eur = "";
     }
+    // Limito el texto a 14, ya que la línea completa tiene 20 espacios. (1-14 -> artículo, 15 -> espacio en blanco, 16-20 -> precio)
     data.texto = datosExtra + "" + data.texto.substring(0, 14);
     data.texto += " " + data.precio + eur;
     try 
