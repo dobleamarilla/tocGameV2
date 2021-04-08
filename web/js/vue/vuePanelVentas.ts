5aa9eca0cff275cb0343af678e3ac240b7aab968
itsS4nty <82sao82@gmail.com>
2021-04-08 14:16:22 +0200

    Visor

--- web/js/vue/vuePanelVentas.ts
+++ web/js/vue/vuePanelVentas.ts
@@ -165,7 +165,8 @@ var vuePanelVentas = new Vue({
             let nombreArticulo = objListadoTeclas.nombreArticulo.nombre;
             let precioArticulo = objListadoTeclas.nombreArticulo.precio;
             precioArticulo = precioArticulo.replace("€", "");
-            ipcRenderer.send('mostrar-visor', {texto: nombreArticulo, precio: precioArticulo, total: toc.getCesta().tiposIva.importe2, dependienta: toc.getCurrentTrabajador().nombre});
+            let totalCesta = toc.getCesta().tiposIva.importe1 + toc.getCesta().importe2 + toc.getCesta().importe3;
+            ipcRenderer.send('mostrar-visor', {texto: nombreArticulo, precio: precioArticulo, total: totalCesta, dependienta: toc.getCurrentTrabajador().nombre});
         },
         resetTeclado()
         {
