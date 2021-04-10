72e449c3b0bc7116c2a5f95f8a511637b7266cb8
itsS4nty <82sao82@gmail.com>
2021-04-10 12:42:13 +0200

    Visor

--- componentes/impresora.ts
+++ componentes/impresora.ts
@@ -335,7 +335,8 @@ var mostrarVisor = function(event, data) {
 		baudRate: 9600,
 		stopBit: 2
 	}); 
-        var options = { encoding: "ISO-8859-16" };
+        //var options = { encoding: "ISO-8859-16" };
+        var options = { encoding: "EUC-JP" };
         var printer = new escpos.Screen(device, options);
         device.open(function () 
         {
